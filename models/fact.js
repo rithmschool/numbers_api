58203127a0697a64fd3d7dc0ccbafe93fb85da29
David Hu <davidhu91@gmail.com>
2012-02-12 13:12:15 -0500

    basic default msgs

--- models/fact.js
+++ models/fact.js
@@ -1,6 +1,9 @@
 var _ = require('underscore');
 var data = require('./data.js');
 
+// TODO: There are a bunch of utils functions here that could be offshored
+// to another file if they're ever needed elsewhere.
+
 /**
  * @return {number} in [min, max) (inclusive-exclusive).
  */
@@ -22,11 +25,13 @@ function clamp(min, max, num) {
 	return Math.max(min, Math.min(max, num));
 }
 
+function randomChoice(array) {
+	return array[randInt(0, array.length)];
+}
+
 function getRandomApiNum(type, options) {
 	// FIXME Should make an effort to return the random number that actually has
 	// an associated fact
-
-	console.log(JSON.stringify(options));
 
 	if (type === 'date') {
 		return randDayOfYear();
@@ -45,18 +50,47 @@ function getRandomApiNum(type, options) {
 	return randInt(min, max + 1);
 }
 
+function getDefaultMsg(number, type) {
+	var mathMsgs = _.map([
+		'an uninteresting number',
+		'a plain old number',
+		'a boring number',
+		'a most unremarkable number',
+		'a number for which only Ramanujan could find an interesting property',
+	], function(val) { return '' + number + ' is ' + val; });
+
+	var defaultMsgs = {
+		'math': mathMsgs,
+		'trivia': mathMsgs,  // TODO Actually come up with trivia defaults
+		'date': ['ERROR: Need to finish mining. Should be no default dates.'],
+		'year': [
+			'nothing remarkable happened.',
+			'the Earth probably made about one revolution around the Sun.',
+			'nothing interesting came to pass.',
+			'we do not know what happened.',
+		],
+	}[type];
+
+	return randomChoice(defaultMsgs);
+}
+
 /**
  * @param number: If type is 'date', then number should be a date.
+ * @param type: Currently supporting {trivia,date,math,year}
  */
 exports.getFact = function(number, type, options) {
 	// TODO query options
+
+	// Default query param options
+	_.defaults(options, {
+	});
 
 	if (number === 'random') {
 		number = getRandomApiNum(type, options);
 	}
 
 	if (type === 'date') {
-		number = '' + (number.getMonth() + 1) + '/' + number.getDay();
+		number = '' + (number.getMonth() + 1) + '/' + number.getDate();
 	}
 
 	// TODO Better error handling
@@ -64,7 +98,7 @@ exports.getFact = function(number, type, options) {
 
 	var ret = data[type][number];
 	if (ret === undefined) {
-		return 'None found for type ' + type + ' of number ' + number;
+		return options['default'] || getDefaultMsg(number, type);
 	} else {
 		return ret;
 	}
