dc5e3e3cc59f93a2d4b9ded5dbc4e43a531ed07c
mduan <duan.mack@gmail.com>
2012-02-11 19:56:43 -0500

    Fix typo

--- html/config.rb
+++ html/config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-@import "compass_twitter_bootstrap";
+require 'compass_twitter_bootstrap'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
