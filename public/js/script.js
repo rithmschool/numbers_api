28252669b013007b6eb3a4bd4e48f1d82f03aaa7
mduan <duan.mack@gmail.com>
2012-02-13 00:51:25 -0500

    fixed api path for examples & searching support
    -> bunch of changes in path from /category/### to ###/category
    -> enter on search box to search, or add hash to url, or choose an
    example

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,62 @@
-$(document).ready(function() {
-  $prev_selected = undefined;
-  $('#search-examples a').click(function(evt) {
-    evt.preventDefault();
+$(function() {
+
+  // TODO: mvc to keep url, selected example, search text, and result in sync
+
+  function update_result(url) {
+    $.ajax({
+      url: url,
+      success: function(data) {
+        var $result = $('#search-result');
+        $result.text(data);
+        $result.removeClass('error');
+      },
+      error: function() {
+        var $result = $('#search-result');
+        $result.text("Invalid url.");
+        $result.addClass('error');
+      }
+    });
+  }
+
+  function update_query(url) {
+    $('#search-text').val(url);
+    update_result(url);
+  }
+
+  (function() {
+    var hash = window.location.hash;
+    if (hash) {
+      hash = hash.substring(1, hash.length);
+      console.log('hash: ', hash);
+      update_query(hash);
+    }
+  })();
+
+  var $prev_selected = undefined;
+  $('#search-examples a').click(function(e) {
     var $this = $(this);
-    var link_path = $this.attr('href');
+    var url = $this.attr('href');
+    url = url.substring(1, url.length);
     if ($prev_selected) {
       $prev_selected.removeClass('selected');
     }
-    $this.parent().addClass('selected');
-    $('#search-text').val(link_path);
-    $('#search-result').text('Random value: ' + new Date());
-    $prev_selected = $this.parent();
+    var $parent = $this.parent();
+    $parent.addClass('selected');
+    $('#search-text').val(url);
+    update_query(url);
+
+    $prev_selected = $parent;
+  });
+
+  var $prev_selected = undefined;
+  $('#search-text').keypress(function(e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+      var $this = $(this);
+      var url = $this.val();
+      window.location.hash = url;
+      update_result(url);
+    }
   });
 });
 
