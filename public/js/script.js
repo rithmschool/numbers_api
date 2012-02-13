eae46a4aef230328a907a40617fbab137cc4962d
mduan <duan.mack@gmail.com>
2012-02-12 21:19:50 -0500

    Working on sandbox part of landing page

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
+  $prev_selected = undefined;
   $('#search-examples a').click(function(evt) {
     evt.preventDefault();
-    var link_path = $(this).attr('href');
-    console.log('link_path', link_path);
+    var $this = $(this);
+    var link_path = $this.attr('href');
+    if ($prev_selected) {
+      $prev_selected.removeClass('selected');
+    }
+    $this.parent().addClass('selected');
     $('#search-text').val(link_path);
     $('#search-result').text('Random value: ' + new Date());
+    $prev_selected = $this.parent();
   });
 });
 
