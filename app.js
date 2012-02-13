340a3b9fe746ff0c4c9104d90cf5b67d87c7fe88
David Hu <davidhu91@gmail.com>
2012-02-12 19:38:54 -0500

    change port to 8124

--- app.js
+++ app.js
@@ -47,5 +47,5 @@ app.get('/', function(req, res) {
 
 // Main
 
-app.listen(3000);
+app.listen(8124);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
