1c2667c7e97e8244b7c083d955e24cc8b2606da8
David Hu <davidhu91@gmail.com>
2012-02-24 01:39:04 -0500

    add x-requested-with to access-control-allow-headers for jq 1.5.1 compatibility

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ var app = module.exports = express.createServer();
 // CORS middleware -- http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
 var allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 };
 
