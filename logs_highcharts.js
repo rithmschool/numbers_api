e9622fa1319c01f91e10e96e4d3e7338415d0bac
David Hu <davidhu91@gmail.com>
2012-03-05 11:47:08 -0500

    cluster histogram by day

--- logs_highcharts.js
+++ logs_highcharts.js
@@ -4,16 +4,16 @@ var _ = require('underscore');
 var UPDATE_FROM_LOGS_FREQUENCY = 1000 * 60 * 5;
 
 var typeTimeHistogram = {
-	'trivia': [],
-	'math': [],
-	'year': [],
-	'date': [],
+	'trivia': {},
+	'math': {},
+	'year': {},
+	'date': {},
 };
 
 function updateStatsFromLogs() {
 	// Clear the histogram first
 	_.each(typeTimeHistogram, function(value, key) {
-		typeTimeHistogram[key] = [];
+		typeTimeHistogram[key] = {};
 	});
 
 	fs.readdir('./logs', function(err, files) {
@@ -21,10 +21,13 @@ function updateStatsFromLogs() {
 			fs.readFile('./logs/' + file, 'utf-8', function(err, contents) {
 				try {
 					_.each(contents.split('\n'), function(logJson) {
+						var logObj = null;
 						try {
-							var logObj = JSON.parse(logJson);
-							reduceStats(logObj);
+							logObj = JSON.parse(logJson);
 						} catch(e) {
+						}
+						if (logObj) {
+							reduceStats(logObj);
 						}
 					});
 				} catch (e) {
@@ -39,10 +42,17 @@ function reduceStats(obj) {
 	var timestamp = obj.time;
 	var query = obj.query;
 	var number = query.number || 'random';
-	var type = query.type;
+	var type = query.type || 'trivia';
 
 	if (timestamp >= (new Date()).getTime() - 1000 * 60 * 60 * 24 * 7) {
-		typeTimeHistogram[type].push([timestamp, 1]);
+		var date = new Date(timestamp);
+		var flooredDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+		var flooredTime = flooredDate.getTime();
+		var hist = typeTimeHistogram[type];
+		if (!hist[flooredTime]) {
+			hist[flooredTime] = 0;
+		}
+		hist[flooredTime] += 1;
 	}
 }
 
@@ -50,5 +60,12 @@ setTimeout(updateStatsFromLogs, 0);
 setInterval(updateStatsFromLogs, UPDATE_FROM_LOGS_FREQUENCY);
 
 exports.getTypeTimeHist = function() {
-	return typeTimeHistogram;
+	var ret = {};
+	_.each(typeTimeHistogram, function(hist, type) {
+		ret[type] = [];
+		_.each(hist, function(value, key) {
+			ret[type].push([key, value]);
+		});
+	});
+	return ret;
 };
