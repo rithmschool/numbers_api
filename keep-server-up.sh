8b7fd95f86e1dcef97305c11a64e2c6c0131aec8
mduan <duan.mack@gmail.com>
2012-03-10 20:14:55 -0500

    comment keep-server-up.sh

--- keep-server-up.sh
+++ keep-server-up.sh
@@ -7,6 +7,7 @@ while :; do
 	instances=$(ps aux | grep -c app.js)
 	# grep, supervisor
 	if [ $instances -lt 3 ]; then
+		# TODO: echos should also be piped to deploy.log as well (currently nohup.out)
 		echo $(date)
 		echo "uh oh, restarting supervisor..."
 		make start >> deploy.log
