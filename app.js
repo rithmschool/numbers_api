b0c1c4de28afc3393913347ebfe9729d61e4fe1b
Matt Lane <lane.matthew@gmail.com>
2020-06-05 14:25:38 -0700

    comment out share statistics

--- app.js
+++ app.js
@@ -26,17 +26,17 @@ const utils = require("./public/js/shared_utils.js");
 // TODO: Get rid of all these try catches, should use forever or something
 // Periodically get # of shares from AddThis API (THANK YOU FOR SANE API, You
 // are so much better than Google Analytics!!!)
-var ADD_THIS_API_HOST = "api.addthis.com";
-var ADD_THIS_API_SHARE_PATH =
-  "/analytics/1.0/pub/shares.json?userid=" +
-  secrets.ADD_THIS_USERNAME +
-  "&password=" +
-  secrets.ADD_THIS_PASSWORD +
-  "&pubid=" +
-  secrets.ADD_THIS_PUBID;
-var GET_NUM_SHARES_INTERVAL_MS = 1000 * 30;
+// var ADD_THIS_API_HOST = "api.addthis.com";
+// var ADD_THIS_API_SHARE_PATH =
+//   "/analytics/1.0/pub/shares.json?userid=" +
+//   secrets.ADD_THIS_USERNAME +
+//   "&password=" +
+//   secrets.ADD_THIS_PASSWORD +
+//   "&pubid=" +
+//   secrets.ADD_THIS_PUBID;
+// var GET_NUM_SHARES_INTERVAL_MS = 1000 * 30;
 var numShares = 15;
-var arguments = process.argv.splice(2);
+// var arguments = process.argv.splice(2);
 
 // Dump all facts data to a directory
 if (_.contains(arguments, "--dump")) {
@@ -45,50 +45,50 @@ if (_.contains(arguments, "--dump")) {
   process.exit(0);
 }
 
-function updateNumShares() {
-  var msg = "";
-  try {
-    https
-      .get(
-        {
-          host: ADD_THIS_API_HOST,
-          path: ADD_THIS_API_SHARE_PATH,
-          headers: {
-            "Cache-Control": "no-cache",
-            Pragma: "no-cache"
-          }
-        },
-        function (res) {
-          res.on("data", function (data) {
-            msg += data.toString();
-            try {
-              // TODO: Get daily number of shares, hourly, etc. and display best value
-              var dataObj = JSON.parse(msg);
-              numShares =
-                _.reduce(
-                  dataObj,
-                  function (accum, val) {
-                    return accum + val["shares"];
-                  },
-                  0
-                ) + 2;
-            } catch (e) {
-              console.log(
-                "Exception handling response from AddThis share:",
-                e.message
-              );
-            }
-          });
-        }
-      )
-      .on("error", function (e) {
-        console.log("Got error: " + e.message);
-      });
-  } catch (e) {
-    console.log("Exception getting number of shares:", e.message);
-  }
-}
-setInterval(updateNumShares, GET_NUM_SHARES_INTERVAL_MS);
+// function updateNumShares() {
+//   var msg = "";
+//   try {
+//     https
+//       .get(
+//         {
+//           host: ADD_THIS_API_HOST,
+//           path: ADD_THIS_API_SHARE_PATH,
+//           headers: {
+//             "Cache-Control": "no-cache",
+//             Pragma: "no-cache"
+//           }
+//         },
+//         function (res) {
+//           res.on("data", function (data) {
+//             msg += data.toString();
+//             try {
+//               // TODO: Get daily number of shares, hourly, etc. and display best value
+//               var dataObj = JSON.parse(msg);
+//               numShares =
+//                 _.reduce(
+//                   dataObj,
+//                   function (accum, val) {
+//                     return accum + val["shares"];
+//                   },
+//                   0
+//                 ) + 2;
+//             } catch (e) {
+//               console.log(
+//                 "Exception handling response from AddThis share:",
+//                 e.message
+//               );
+//             }
+//           });
+//         }
+//       )
+//       .on("error", function (e) {
+//         console.log("Got error: " + e.message);
+//       });
+//   } catch (e) {
+//     console.log("Exception getting number of shares:", e.message);
+//   }
+// }
+// setInterval(updateNumShares, GET_NUM_SHARES_INTERVAL_MS);
 
 const nodeEnv = process.env.NODE_ENV || "development";
 const app = express();
