b6c9e919beb78ef2bd371436d4b3b52523fae4fd
stephenisau <sfcstau@gmail.com>
2020-08-31 00:43:05 -0700

    Add integration tests for app.test.js

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -1,8 +1,24 @@
 const app = require("../../app");
 const request = require("supertest");
 const fact = require("../../models/fact");
+const { createWriteStream } = require("../../__mocks__/fs");
 
 describe("app.js", function () {
+  beforeEach(() => {
+    jest.mock("fs", () => {
+      createWriteStream: (filepath, ...args) => {
+        return {
+          write: (dataStream) => console.log("Writing..."),
+          destroySoon: () => console.log("Destroying stream..."),
+        };
+      };
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe("GET /", function () {
     test("GET request responds with a 200 status code", async function (done) {
       const response = await request(app).get("/");
@@ -25,28 +41,13 @@ describe("app.js", function () {
   });
 
   describe("POST /submit", function () {
-    test("it should respond with a 200 status code", async function (done) {
+    test("POST request should respond with a 200 status code", async function (done) {
       const response = await request(app).post("/submit");
       expect(response.statusCode).toBe(200);
       done();
     });
 
-    test("it should return request body to user", async function (done) {
-      let data = {
-        trivia: true,
-        number: 5,
-        fact: "5 is an odd number",
-      };
-      const response = await request(app)
-        .post("/submit")
-        .send(data)
-        .set("Content-Type", "application/json");
-
-      expect(response.body).toEqual(data);
-      done();
-    });
-
-    test("it should not return data to user if no headers are set", async function (done) {
+    test("POST request should not return data to user if no headers are set", async function (done) {
       let data = {
         trivia: true,
         number: 5,
