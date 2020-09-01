57162989c79d03e61f8c595b8f2458211caac396
stephenisau <sfcstau@gmail.com>
2020-09-01 15:32:45 -0700

    Update test suite descriptions

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -1,6 +1,7 @@
 const app = require("../../app");
 const request = require("supertest");
 const fact = require("../../models/fact");
+const api = require("../../routes/api");
 
 describe("app.js", function () {
   describe("GET /", function () {
@@ -17,28 +18,42 @@ describe("app.js", function () {
     });
 
     test("GET request should call fact.getFact()", async function (done) {
-      let getFact = jest.spyOn(fact, "getFact");
+      const getFact = jest.spyOn(fact, "getFact");
       const response = await request(app).get("/");
       expect(getFact).toHaveBeenCalled();
       done();
     });
   });
 
-  describe("POST /submit", function () {
+  describe("POST => /submit", function () {
     test("POST request should respond with a 200 status code", async function (done) {
       const response = await request(app).post("/submit");
-      expect(response.statusCode).toBe(200);
+      const { statusCode } = response;
+      expect(statusCode).toBe(200);
       done();
     });
 
-    test("POST request should not return data to user if no headers are set", async function (done) {
+    test("POST /submit request should not return data to user if no headers are set", async function (done) {
       let data = {
         trivia: true,
         number: 5,
         fact: "5 is an odd number",
       };
-      const response = await request(app).post("/submit");
+      const response = await request(app).post("/submit").send(data);
       expect(Object.keys(response.body).length).toEqual(0);
+      done();
+    });
+
+    test("POST /submit request should call appendToFile method in our router", async function (done) {
+      const appendToFile = jest.spyOn(api, "appendToFile");
+      let data = {
+        trivia: true,
+        number: 5,
+        fact: "5 is an odd number",
+      };
+
+      const response = await request(app).post("/submit").send(data);
+      expect(appendToFile).toHaveBeenCalled();
       done();
     });
   });
