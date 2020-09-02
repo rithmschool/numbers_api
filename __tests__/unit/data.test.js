ba08daf2f1635f23fc9c7975239791a6ecf8b28a
stephenisau <sfcstau@gmail.com>
2020-09-02 15:11:21 -0700

    Refactored test descriptions and removed bad tests

--- __tests__/unit/data.test.js
+++ __tests__/unit/data.test.js
@@ -6,10 +6,11 @@ const {
 
 jest.mock("fs");
 
-describe("data.js functions", function () {
+describe("Unit testing functions in `models/data.js`", function () {
   let consoleSpy;
-
-  const MOCK_INFO = {
+  let data;
+  let callback;
+  const MOCKINFO = {
     "/path/to/norm/good/file1.txt": {
       "213": [
         {
@@ -35,8 +36,8 @@ describe("data.js functions", function () {
    * Setting in-memory data for dummy data in our mocks directory
    */
   beforeEach(function () {
-    let stringify = JSON.stringify(MOCK_INFO);
-    require("fs").__setMockFiles(MOCK_INFO);
+    let stringify = JSON.stringify(MOCKINFO);
+    require("fs").__setMockFiles(MOCKINFO);
     require("fs").__setMockFileContent(stringify);
     consoleSpy = jest.spyOn(console, "warn");
   });
@@ -47,56 +48,55 @@ describe("data.js functions", function () {
   });
 
   describe("reader_norm function", function () {
+    beforeEach(function () {
+      data = {};
+      callback = jest.fn((el) => el);
+    });
+
     test("Logs error message to the console", function () {
-      let data = {};
       let badPath = "/path/to/norm/bad/";
-      let callback = jest.fn((el) => el);
       reader_norm(data, badPath, callback);
       expect(consoleSpy).toHaveBeenCalled();
     });
 
     test("Given a bad pathname, no data is added to our input object", function () {
-      let data = {};
       let badPath = "/path/to/norm/bad/";
-      let callback = jest.fn((el) => el);
       reader_norm(data, badPath, callback);
       let keys = Object.keys(data);
       expect(keys.length).toEqual(0);
     });
 
     test("Correctly adds data to our input object given a correct pathname", function () {
-      let data = {};
       let pathname = "/path/to/norm/good/";
-      let cb = jest.fn((el) => el);
-      reader_norm(data, pathname, cb);
+      reader_norm(data, pathname, callback);
       let keys = Object.keys(data);
       expect(keys.length).toBeGreaterThan(0);
     });
   });
 
   describe("reader_manual function", function () {
-    test("Adds data to the correct category in our input object", function () {
-      let data = { t: {}, y: {}, m: {}, d: {} };
+    beforeEach(function () {
+      data = { t: {}, y: {}, m: {}, d: {} };
+      callback = jest.fn((el) => el);
+    });
+
+    // This test checks that we've added data to the appropriate key value input
+    test("checks that we've added data to the appropriate key value store in our input object", function () {
       let path = "/path/to/manual/";
-      let cb = jest.fn((el) => el);
-      reader_manual(data, path, cb);
+      reader_manual(data, path, callback);
       let triviaKeys = Object.keys(data["t"]);
       expect(triviaKeys.length).toBeGreaterThan(0);
     });
 
-    test("Logs error message to the console", function () {
-      let data = { t: {}, y: {}, m: {}, d: {} };
+    test("Logs error message to the console when given a bad pathname", function () {
       let path = "/path/to/norm/bad/";
-      let cb = jest.fn((el) => el);
-      reader_manual(data, path, cb);
+      reader_manual(data, path, callback);
       expect(consoleSpy).toHaveBeenCalled();
     });
 
-    test("no data is added if given a bad pathname", function () {
-      let data = { t: {}, y: {}, m: {}, d: {} };
+    test("No data is added to our input object when given a bad pathname", function () {
       let path = "/path/to/norm/bad/";
-      let cb = jest.fn((el) => el);
-      reader_manual(data, path, cb);
+      reader_manual(data, path, callback);
       for (let key in data) {
         const keys = Object.keys(data[key]);
         expect(keys.length).toEqual(0);
@@ -105,7 +105,7 @@ describe("data.js functions", function () {
   });
 
   describe("normalize_common function", function () {
-    test("removes capitalization when 'NP' tag is not passed in", function () {
+    test("Sets first letter to lowercase character when 'NP' tag is not passed in", function () {
       let element = {
         date: "August 4",
         text:
@@ -117,7 +117,7 @@ describe("data.js functions", function () {
       expect(element.text[0]).not.toEqual(element.text[0].toUpperCase());
     });
 
-    test("returns undefined if self key is set to true", function () {
+    test("function returns undefined if `self` key is set to true", function () {
       let element = {
         date: "August 4",
         text: "This is some text",
@@ -129,28 +129,13 @@ describe("data.js functions", function () {
 
     test("returns undefined if invalid character is passed in", function () {
       let element = {
-        date: "Augst 4",
+        date: "August 4",
         text: "invalid!!!@#)((*!@)$",
         self: false,
         pos: "NP",
       };
 
       expect(normalize_common(element)).toBeUndefined();
-    });
-
-    test("Doesn't modify input text value if NP tag is passed in", function () {
-      let element = {
-        date: "Augst 4",
-        text:
-          "118 t the number of decibels of the loudest burp, held by record-holder Paul Hunn, which is as loud as a chainsaw",
-        self: false,
-        pos: "NP",
-      };
-      let newElement = normalize_common(element);
-      for (let key in newElement) {
-        const currElement = newElement[key];
-        expect(currElement).toEqual(element[key]);
-      }
     });
   });
 });
