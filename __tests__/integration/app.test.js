939bddd02c6087701c39c6676a18b9b22d541b62
stephenisau <sfcstau@gmail.com>
2020-09-16 15:59:04 -0700

    update integration tests

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -1,44 +1,55 @@
 const app = require("../../app");
 const request = require("supertest");
 const fact = require("../../models/fact");
+const api = require("../../routes/api");
 
-describe("app.js", function () {
+describe("Integration/unit testing app.js", function () {
   describe("GET /", function () {
-    test("GET request responds with a 200 status code", async function (done) {
+    test("GET request responds with a 200 status code and returns HTML", async function (done) {
       const response = await request(app).get("/");
-      expect(response.statusCode).toBe(200);
-      done();
-    });
-
-    test("GET request responds with html", async function (done) {
-      const response = await request(app).get("/");
-      expect(response.text).toContain("<!DOCTYPE html>");
+      const { statusCode, text } = response;
+      expect(statusCode).toBe(200);
+      expect(text).toContain("<!DOCTYPE html>");
       done();
     });
 
     test("GET request should call fact.getFact()", async function (done) {
-      let getFact = jest.spyOn(fact, "getFact");
+      const getFact = jest.spyOn(fact, "getFact");
       const response = await request(app).get("/");
       expect(getFact).toHaveBeenCalled();
       done();
     });
   });
 
-  describe("POST /submit", function () {
+  describe("POST => /submit", function () {
     test("POST request should respond with a 200 status code", async function (done) {
       const response = await request(app).post("/submit");
-      expect(response.statusCode).toBe(200);
+      const { statusCode } = response;
+      expect(statusCode).toBe(200);
       done();
     });
 
-    test("POST request should not return data to user if no headers are set", async function (done) {
+    test("POST /submit request should not return data to user if no headers are set", async function (done) {
       let data = {
         trivia: true,
         number: 5,
         fact: "5 is an odd number",
       };
-      const response = await request(app).post("/submit");
+      const response = await request(app).post("/submit").send(data);
       expect(Object.keys(response.body).length).toEqual(0);
+      done();
+    });
+
+    test("POST /submit request should call appendToFile method in our router", async function (done) {
+      const appendToFile = jest.spyOn(api, "appendToFile");
+      let data = {
+        trivia: true,
+        number: 5,
+        fact: "5 is an odd number",
+      };
+
+      const response = await request(app).post("/submit").send(data);
+      expect(appendToFile).toHaveBeenCalled();
       done();
     });
   });
