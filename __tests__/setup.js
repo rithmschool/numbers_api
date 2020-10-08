ed0664f0456f0757a8680fcf4dd68dc5e0a471cf
stephenisau <sfcstau@gmail.com>
2020-10-08 15:24:12 -0700

    Update setup.js

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,12 +1,7 @@
 // Configuring console messages to run silently while testing with jest
 // https://medium.com/swlh/6-ways-to-run-jest-test-cases-silently-67d2fead8c11
-
 global.console = {
   ...console,
-  log: jest.fn(),
-  debug: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
 };
-
-jest.spyOn(global.console, "log").mockImplementation(jest.fn());
-jest.spyOn(global.console, "warn").mockImplementation(jest.fn());
-jest.spyOn(global.console, "error").mockImplementation(jest.fn());
