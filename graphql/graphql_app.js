c356e4bab8699b08f7ce3bb42a57e6ac2775c4e8
Jessica <jessica@jessicas-mbp.lan>
2021-05-10 16:42:47 -0700

    add graphiql to route for browser interface

--- graphql/graphql_app.js
+++ graphql/graphql_app.js
@@ -4,14 +4,18 @@ const schema = require("./schema/schema");
 
 const app = new express();
 
-app.use('/graphql', graphqlHTTP({
-    schema
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 /** year: 24
  * [
  * {"text": "The Roman war against Numidia and Mauretania ends with their annexation.", "self": false, "pos": "DET"},
- *  {"text": "Servius Cornelius Cethegus and Lucius Visellius Varro become consuls.", "self": false, "pos": "NP"}, 
+ *  {"text": "Servius Cornelius Cethegus and Lucius Visellius Varro become consuls.", "self": false, "pos": "NP"},
  * {"text": "Charmides becomes Archon of Athens.", "self": false, "pos": "NP"},
  *  {"text": "Tacfarinas' revolt in Africa is repressed.", "self": false, "pos": "NP"},
  *  {"text": "Philo declares that the Old Testament is the eternal law of God.", "self": false, "pos": "NP"}
@@ -20,11 +24,11 @@ app.use('/graphql', graphqlHTTP({
 
 /** date: 24
  * [
- * {"text": "The Council of Basel suspends Pope Eugene IV as Prelate of Ethiopia, arrives at Massawa from Goa.", "self": false, "pos": "DET", "year": 1438}, 
+ * {"text": "The Council of Basel suspends Pope Eugene IV as Prelate of Ethiopia, arrives at Massawa from Goa.", "self": false, "pos": "DET", "year": 1438},
  * {"text": "Charles VII Albert becomes Holy Roman Emperor.", "self": false, "pos": "NP", "year": 1742},
  *  {"text": "The University of Calcutta is formally founded as the first fully-fledged university in south Asia.", "self": false, "pos": "DET", "year": 1857}
  * ]
- * 
+ *
  */
 
 /** math: 24
