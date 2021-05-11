e1dd6dd6df12062110200be032e6f24ebfc8a176
Jessica <jessica@jessicas-mbp.lan>
2021-05-11 14:46:14 -0700

    remove unused code

--- graphql/schema/schema.js
+++ graphql/schema/schema.js
@@ -1,6 +1,5 @@
 const graphql = require("graphql");
 const { getAllFacts } = require("../../models/fact.js");
-const _ = require("underscore");
 
 const {
   GraphQLObjectType,
@@ -9,25 +8,6 @@ const {
   GraphQLList,
   GraphQLSchema,
 } = graphql;
-
-// let numberFacts = getAllFacts(24);
-
-let dummyData = [
-  {
-    number: 24,
-    math: ["24math1", "24marth1"],
-    trivia: ["24trv"],
-    date: ["24date1", "24date2"],
-    year: ["24year"],
-  },
-  {
-    number: 25,
-    math: ["25math1", "25math2"],
-    trivia: ["25trv"],
-    date: ["25date1", "25date2"],
-    year: ["24year"],
-  },
-];
 
 const NumberType = new GraphQLObjectType({
   name: "Number",
@@ -55,10 +35,8 @@ const RootQuery = new GraphQLObjectType({
       type: NumberType,
       args: { number: { type: GraphQLInt } },
       resolve(parent, args) {
-        // code to get data from db / other source
         let num = args.number;
         return getAllFacts(num);
-        // return _.find(dummyData, { number: args.number });
       },
     },
   },
