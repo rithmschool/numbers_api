144ebc4132e45d4e2e733ec807917f951d835683
Jessica <jessica@jessicas-mbp.lan>
2021-05-13 11:15:36 -0700

    change getFacts query to getNumberFacts query
    for clarificiation
    also adding a second endpoint for getTypeFacts, can distinguish between the two queries

--- graphql/schema/Resolvers.js
+++ graphql/schema/Resolvers.js
@@ -1,12 +1,12 @@
 const { getAllFacts } = require("../../models/fact.js");
 
 const resolvers = {
-    Query: {
-        getFacts(parent, args){
-            let num = args.number;
-            return getAllFacts(num);
-        }
-    }
-}
+  Query: {
+    getNumberFacts(parent, args) {
+      let num = args.number;
+      return getAllFacts(num);
+    },
+  },
+};
 
 module.exports = { resolvers };
--- graphql/schema/TypeDefs.js
+++ graphql/schema/TypeDefs.js
@@ -1,22 +1,18 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Number {
-      number: Int!
-      math: [String!]!
-      trivia: [String!]!
-      date: [String!]!
-      year: [String!]!
-    }
+  type Number {
+    number: Int!
+    math: [String!]!
+    trivia: [String!]!
+    date: [String!]!
+    year: [String!]!
+  }
 
-    # Queries
-    type Query {
-      getFacts(number: Int!): Number!
-    }
+  # Queries
+  type Query {
+    getNumberFacts(number: Int!): Number!
+  }
 `;
 
 module.exports = { typeDefs };
-
-
-
-    
