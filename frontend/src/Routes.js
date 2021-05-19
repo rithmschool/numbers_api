0f296747bff2f99fec516f7aeec68250a4a2a131
Gary Rivera <a.gary.rivera@gmail.com>
2021-05-19 08:17:51 -0700

    added a catch-all route for invalid urls

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,26 +1,28 @@
 import {Switch, Route, Redirect} from "react-router-dom"
 import Home from "./Home"
 import APIResponse from "./APIResponse"
+import NotFound from "./NotFound"
 
 /*;
-Routes to either website OR api response
-
-Props: 
-- None
-State:
-- None
-
-(Route) -> Home
-        -> APIResponse
+* Routes to either website OR api response
+*
+* Props: 
+* - None
+* State:
+* - None
+* 
+* (Route) -> Home
+*         -> APIResponse
 */
 
 function Routes() {
   return (
     <div>
       <Switch>
-      <Route path={`/fact/:parameter`}><Home/></Route>
-      <Route path={`/api/:parameter`}><APIResponse/></Route>
-      {/* <Redirect to='/fact/42'/> */}
+        <Route path={`/fact/:parameter`}><Home/></Route>
+        <Route path={`/api/:parameter`}><APIResponse/></Route>
+        <Route exact path={`/`}><Home/></Route>
+        <Route><NotFound /></Route>
       </Switch>
     </div>
   )
