9b2ebe135a505b057e5c971ba55d872a030f643c
Ben Hong <benhong654@gmail.com>
2021-05-20 15:28:54 -0700

    created tests for getVisitors query

--- wikidata/__tests__/gqlQueries.test.js
+++ wikidata/__tests__/gqlQueries.test.js
@@ -75,3 +75,332 @@ describe("Test attractions/museums queries", () => {
     expect(result).toEqual([]);
   });
 });
+
+describe("Test visitor graphQL queries", () => {
+
+  beforeEach(() => {
+    tester = new EasyGraphQLTester(schema);
+  });
+
+  it ("returns the greater than equal to results correctly for attractions", async () => {
+    const query = `
+      {
+        getVisitors(type: "attractions", operator:">=", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Attraction 2",
+        visitors: 2,
+        coordinates: "Point(-77.03655 38.897669444)",
+      },
+      {
+        name: "Attraction 3",
+        visitors: 19,
+        coordinates: "Point(60.72222 56.893194)",
+      }
+    ])
+  })
+
+  it ("returns the greater than equal to results correctly for museums", async () => {
+    const query = `
+      {
+        getVisitors(type: "museums", operator:">=", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Museum 2",
+        visitors: 2,
+        coordinates: "Point(-77.03655 38.897669444)",
+      },
+      {
+        name: "Museum 3",
+        visitors: 19,
+        coordinates: "Point(60.72222 56.893194)",
+      }
+    ])
+  })
+
+  it ("returns the less than equal to results correctly for attractions", async () => {
+    const query = `
+      {
+        getVisitors(type: "attractions", operator:"<=", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Attraction 1",
+        visitors: 0,
+        coordinates: "Point(60.572472222 56.893194444)",
+      },
+      {
+        name: "Attraction 2",
+        visitors: 2,
+        coordinates: "Point(-77.03655 38.897669444)",
+      },
+    ])
+  })
+
+  it ("returns the less than equal to results correctly for museums", async () => {
+    const query = `
+      {
+        getVisitors(type: "museums", operator:"<=", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Museum 1",
+        visitors: 0,
+        coordinates: "Point(60.572472222 56.893194444)",
+      },
+      {
+        name: "Museum 2",
+        visitors: 2,
+        coordinates: "Point(-77.03655 38.897669444)",
+      },
+    ])
+  })
+
+  it ("returns the less than results correctly for attractions", async () => {
+    const query = `
+      {
+        getVisitors(type: "attractions", operator:"<", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Attraction 1",
+        visitors: 0,
+        coordinates: "Point(60.572472222 56.893194444)",
+      },
+    ])
+  })
+
+  it ("returns the less than results correctly for museums", async () => {
+    const query = `
+      {
+        getVisitors(type: "museums", operator:"<", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Museum 1",
+        visitors: 0,
+        coordinates: "Point(60.572472222 56.893194444)",
+      },
+    ])
+  })
+
+  it ("returns the greater than results correctly for attractions", async () => {
+    const query = `
+      {
+        getVisitors(type: "attractions", operator:">", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Attraction 3",
+        visitors: 19,
+        coordinates: "Point(60.72222 56.893194)",
+      },
+    ])
+  })
+
+  it ("returns the greater than results correctly for museums", async () => {
+    const query = `
+      {
+        getVisitors(type: "museums", operator:">", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Museum 3",
+        visitors: 19,
+        coordinates: "Point(60.72222 56.893194)",
+      },
+    ])
+  })
+
+  it ("returns the equal to results correctly for attractions", async () => {
+    const query = `
+      {
+        getVisitors(type: "attractions", operator:"===", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Attraction 2",
+        visitors: 2,
+        coordinates: "Point(-77.03655 38.897669444)",
+      },
+    ])
+  })
+
+  it ("returns the equal to results correctly for museums", async () => {
+    const query = `
+      {
+        getVisitors(type: "museums", operator:"===", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual([
+      {
+        name: "Museum 2",
+        visitors: 2,
+        coordinates: "Point(-77.03655 38.897669444)",
+      },
+    ])
+  })
+
+  it ("returns the 'plus' results correctly for attractions", async () => {
+    const query = `
+      {
+        getVisitors(type: "attractions", operator:"+", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual(
+      [
+        {
+          name: 'Attraction 1',
+          visitors: 0,
+          coordinates: 'Point(60.572472222 56.893194444)'
+        },
+        {
+          name: 'Attraction 2',
+          visitors: 2,
+          coordinates: 'Point(-77.03655 38.897669444)'
+        },
+        {
+          name: 'Attraction 3',
+          visitors: 19,
+          coordinates: 'Point(60.72222 56.893194)'
+        }
+      ]
+    )
+  })
+
+  it ("returns the 'plus' results correctly for museums", async () => {
+    const query = `
+      {
+        getVisitors(type: "museums", operator:"+", count: 2) {
+          name
+          visitors
+          coordinates
+        }
+      }
+    `
+
+    let result = await tester.graphql(query);
+    result = result.data.getVisitors;
+
+    expect(result).toEqual(
+      [
+        {
+          name: 'Museum 1',
+          visitors: 0,
+          coordinates: 'Point(60.572472222 56.893194444)'
+        },
+        {
+          name: 'Museum 2',
+          visitors: 2,
+          coordinates: 'Point(-77.03655 38.897669444)'
+        },
+        {
+          name: 'Museum 3',
+          visitors: 19,
+          coordinates: 'Point(60.72222 56.893194)'
+        }
+      ]
+    )
+  })
+})
+
+
